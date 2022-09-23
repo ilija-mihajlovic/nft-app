1e382735ed7402fb867a4ab563ac7e3d25b04dd8
Ilija Mihajlovic <iilija879a@gmail.com>
2022-09-23 20:23:02 +0200

    startup

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Text, View, SafeAreaView, FlatList } from 'react-native'
+
+import { COLORS, NFTData } from '../constants'
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1}}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
