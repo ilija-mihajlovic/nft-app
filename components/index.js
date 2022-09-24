c0be8a6f46959463ecb2b75a93926edc9c67fc36
Ilija Mihajlovic <iilija879a@gmail.com>
2022-09-24 10:48:26 +0200

    finished

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as HomeHeader } from './HomeHeader'
+export { default as FocusedStatusBar } from './FocusedStatusBar'
+export { default as NFTCard } from './NFTCard'
+export { default as DetailsDesc } from './DetailsDesc'
+export { default as DetailsBid } from './DetailsBid'
